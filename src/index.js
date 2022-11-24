4d89e11d87248838883d96d9d416b8859f0e2911
Jay Patel <pjpatel1012@gmail.com>
2022-11-24 18:46:24 -0500

    adding all src from initial tutorial

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <div>hello again</div>
     <App />
   </React.StrictMode>
 );
