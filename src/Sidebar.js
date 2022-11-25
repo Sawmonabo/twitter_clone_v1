e0055ccae5c2f9439cef6d9ef9991bff6139da26
Sawmonabo <77422313+Sawmonabo@users.noreply.github.com>
2022-11-25 17:56:55 -0500

    Icon changes

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Sidebar.css";
 import SidebarOption from "./SidebarOption";
-import TwitterIcon from "@mui/icons-material/Twitter";
 import HomeIcon from "@mui/icons-material/Home";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
@@ -11,11 +10,20 @@ import ListAltIcon from "@mui/icons-material/ListAlt";
 import PermIdentityIcon from "@mui/icons-material/PermIdentity";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { Button } from "@mui/material";
+import { Icon } from "@mui/material";
+// import TwitterIcon from "@mui/icons-material/Twitter";
+import TwiiterLogo from './twitter_clone_logo.svg'
+
+export const TwitterIcon = () => (
+    <Icon>
+        <img src={TwiiterLogo} height={30} width={30}/>
+    </Icon>
+)
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <TwitterIcon className="sidebar__twitterIcon" />
+      <TwitterIcon Icon="sidebar__twitterIcon" text="Logo" />
       <SidebarOption Icon={HomeIcon} text="Home" active={true} />
       <SidebarOption Icon={SearchIcon} text="Explore" />
       <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
