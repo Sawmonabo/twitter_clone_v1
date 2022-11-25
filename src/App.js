53bd914b7c88993f95731d994f956474b0aa4396
Jay Patel <pjpatel1012@gmail.com>
2022-11-24 19:11:23 -0500

    fix app to show actual twitter clone

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import './App.css';
 
-import Feed from "./Feed";
+//import Feed from "./Feed";
 import Sidebar from "./Sidebar";
 import Widgets from "./Widgets";
 
 function App() {
   return (
-    <div className="App">
-      Learn React
+    <div className="app">
+      <Sidebar />
+      <Widgets />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>hello again</div>
     <App />
   </React.StrictMode>
 );
