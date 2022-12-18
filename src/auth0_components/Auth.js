56f45ee19e1a64b6f7827e9cd9fce0f8a6e19a5b
Sawmonabo <77422313+Sawmonabo@users.noreply.github.com>
2022-12-18 17:46:52 -0500

    .

--- src/auth0_components/Auth.js
+++ src/auth0_components/Auth.js
@@ -1,27 +1,14 @@
-import React, { useEffect, useState } from "react";
 import LoginButton from "./LoginButton";
-import { useAuth } from "../lib/wundergraph";
 import './Auth.css';
 
 
 function Auth() {
-  const [errorMessages, setErrorMessages] = useState({});
-  const [isSubmitted, setIsSubmitted] = useState(false);
-
-    let [authMode, setAuthMode] = useState("signin")
-  
-    const changeAuthMode = () => {
-      setAuthMode(authMode === "signin" ? "signup" : "signin")
-    }
-
-    const { login } = useAuth();  
-   
     return (
       <div className="Auth-form-container">
         <div className="Auth-form">
           <div className="Auth-form-content">
             <h3 className="Auth-form-title">Twitter-Clone Login</h3>
-                <LoginButton/>    
+            <LoginButton/>    
           </div>
         </div>
       </div>
